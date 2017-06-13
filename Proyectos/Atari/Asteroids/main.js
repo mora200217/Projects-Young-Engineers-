c4343b696f36d188fed5e3a3a82c44b38042ef02
Andres Morales <mora200217@hotmail.com>
2017-06-13 10:58:04 -0500

    Create ship structure. Render function

--- Proyectos/Atari/Asteroids/main.js
+++ Proyectos/Atari/Asteroids/main.js
@@ -1,3 +1,6 @@
+var height = windowHeight;
+var width = windowWidth;
+
 function setup(){
   // Defining Initial Configuration
   var DIFF_INIT = 4;
@@ -6,4 +9,20 @@ function setup(){
 
 function draw(){
   background(0);
+  ship = new Ship();
+  ship.render();
+}
+
+function Ship(){
+  this.pos = createVector(width/2, height/2)
+  this.r = 20; // Radius
+  this.heading = 0; // Heading of the Ship
+  // RENDER SHIP
+  this.render = function(){
+    noFill();
+    stroke(255);
+    translate(this.pos.x, this.pos.y);
+    triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+  }
+
 }
