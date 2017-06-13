f63b70c615f952b6a0cd5dc6de070c85faf6edb1
Andres Morales <mora200217@hotmail.com>
2017-06-13 10:49:04 -0500

    Main Structure Modified. Libraries imported

---
+++ Proyectos/Atari/Asteroids/main.js
@@ -0,0 +1,9 @@
+function setup(){
+  // Defining Initial Configuration
+  var DIFF_INIT = 4;
+  createCanvas(windowWidth - DIFF_INIT, windowHeight - DIFF_INIT);
+}
+
+function draw(){
+  background(0);
+}
